d7d5d2f5742b950935449be762cdefe9f32a39a6
kimberlynZ-prog <kimberlynlzerpa@gmail.com>
2026-02-05 23:07:36 +0000

    Generador de cartas terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+              //   0    1    2    3    4    5    6    7    8     9    10   11   12
+  const suits = [
+    { name: "heart", symbol: "♥" },
+    { name: "spade", symbol: "♠" },
+    { name: "club", symbol: "♣" },
+    { name: "diamond", symbol: "♦" }
+  ];
+
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  const card = document.getElementById("card");
+  const value = document.getElementById("value");
+  const topSuit = document.getElementById("top-suit");
+  const bottomSuit = document.getElementById("bottom-suit");
+
+  card.className = "card " + randomSuit.name;
+  value.textContent = randomValue;
+  topSuit.textContent = randomSuit.symbol;
+  bottomSuit.textContent = randomSuit.symbol;
 };
+
